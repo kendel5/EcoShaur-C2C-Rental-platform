e5b4f9042fb74aa6b9da684c5d281f3255865a91
ihatelua <54477695+ihatelua@users.noreply.github.com>
2020-07-01 11:53:36 +0900

    update

--- ecoshaur/src/main/java/com/cafe24/ecoshaur/HomeController.java
+++ ecoshaur/src/main/java/com/cafe24/ecoshaur/HomeController.java
@@ -1,23 +1,39 @@
-
 package com.cafe24.ecoshaur;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-  // http://localhost:9090/ecoshaur/
-  @RequestMapping(value = "/", method=RequestMethod.GET)
-  public ModelAndView home() {
-    ModelAndView mav = new ModelAndView();
-    mav.setViewName("redirect:index.do");
-    return mav;
-  }//home() end
-  
-}// class end
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
